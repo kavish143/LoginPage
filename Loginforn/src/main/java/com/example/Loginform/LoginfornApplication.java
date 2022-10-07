d85901be9bdcd0e0cb99efe6b01daad2287a5b08
Kavish Bhoyar <97974971+kavish143@users.noreply.github.com>
2022-10-07 23:30:08 +0530

    Add files via upload

---
+++ Loginforn/src/main/java/com/example/Loginform/LoginfornApplication.java
@@ -0,0 +1,13 @@
+package com.example.Loginform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginfornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginfornApplication.class, args);
+	}
+
+}
